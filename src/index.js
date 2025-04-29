928e74101a7bf10bddd520d9cf55940e67db0766
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-30 08:56:20 +0900

    Add localStorage function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import './style.css'
+
+document.addEventListener("DOMContentLoaded", () => {
+    const inputBox = document.getElementById("input-box");
+    const listContainer = document.getElementById("list-container");
+    const addButton = document.querySelector("button");
+
+    function addTask() {
+        if (inputBox.value === '') {
+            alert("Got nothing to do?");
+        } 
+        else {
+            let li = document.createElement("li");
+            li.textContent = inputBox.value;
+            listContainer.appendChild(li);
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
+            inputBox.value = '';
+        }
+    }
+
+    addButton.addEventListener("click", addTask);
+    inputBox.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") addTask();
+    });
+
+    listContainer.addEventListener("click", function(e) {
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+            saveTask();
+        }
+        else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+            saveTask();
+        }
+    }, false);
+
+    function saveTask() {
+        localStorage.setItem("data", listContainer.innerHTML);
+    }
+
+    function showTask() {
+        listContainer.innerHTML = localStorage.getItem("data");
+    }
+
+    showTask();
+});
