3a06c06108524be1b0d13da12d4d83f0d6fa721a
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-30 11:22:12 +0900

    Assign default category

--- src/taskManager.js
+++ src/taskManager.js
@@ -1,17 +1,22 @@
 export function addTask(titleBox, categoryBox, dateBox, priorityBox, 
     descriptionBox, listContainer) {
-    if (titleBox.value.trim() === '') {
-        alert("Got nothing to do?");
-    }
+    let title = titleBox.value;
+    let category = categoryBox.value.trim() || 'General';
+
+    if (title.trim() === '') alert("Got nothing to do?");
     else {
+        const taskContent = `${title} (${category}/${dateBox.value}/${priorityBox.value})`;
         const li = document.createElement("li");
-        const taskContent = `${titleBox.value} (${categoryBox.value}/${dateBox.value}/${priorityBox.value})`;
-        li.textContent = taskContent;
         const span = document.createElement("span");
+        
+        li.textContent = taskContent;
         span.innerHTML = "\u00d7";
         li.appendChild(span);
         listContainer.appendChild(li);
-        titleBox.value = '';
+
+        title = '';
+        category = '';
+
         saveTask(listContainer);
     }
 }
